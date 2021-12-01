cf14915762318743695c144c570ce49b875cb935
Matt Katz <mykatz01@gmail.com>
2021-11-30 23:42:40 -0800

    Build model function

--- README.md
+++ README.md
@@ -3,8 +3,9 @@
 ## Run steps:
 
 1. Download feature source code.
-2. Set OS env var `FEATURE_PATH` to feature source code folder path
-3. Write the rest of the code
-4. Run
+2. Set OS env var `PDB_DIR` to folder path of pdbs and `FEATURE_DIR` to feature/data, `DSSP_DIR` etc
+3. Set OS env var `FEATURE_PATH` to feature source code folder path
+4. Write the rest of the code
+5. Run
 
 
--- test/feature.py
+++ test/feature.py
@@ -3,6 +3,7 @@ Program for bulk testing FEATURE models with different parameters.
 """
 
 import os
+import tempfile
 
 FEATURE_PATH = os.getenv("FEATURE_PATH") or "./feature"
 
@@ -10,7 +11,37 @@ FEATURIZE = f"{FEATURE_PATH}/bin/featurize"
 BUILDMODEL = f"{FEATURE_PATH}/bin/buildmodel"
 SCOREIT = f"{FEATURE_PATH}/bin/scoreit"
 
+# Training examples paths
+POSITIVE_TRAINING_EXAMPLES = "/home/katz/Code/cs279proj/example/trypsin_ser_og.pos.ptf"
+NEGATIVE_TRAINING_EXAMPLES = "/home/katz/Code/cs279proj/example/trypsin_ser_og.neg.ptf"
+
 # Parameters to test
 NUM_SHELLS = [6]
 SHELL_WIDTH = [1.25]
 NUM_BINS = [5]
+
+
+def build_feature_model(num_shells, shell_width, num_bins):
+
+    out_dir = f"out/{num_shells}-{shell_width}-{num_bins}"
+    os.makedirs(out_dir)
+
+    os.system(
+        f"{FEATURIZE} -n {num_shells} -w {shell_width} -P {POSITIVE_TRAINING_EXAMPLES} > {out_dir}/pos.ff"
+    )
+    os.system(
+        f"{FEATURIZE} -n {num_shells} -w {shell_width} -P {NEGATIVE_TRAINING_EXAMPLES} > {out_dir}/neg.ff"
+    )
+
+    os.system(
+        f"{BUILDMODEL} -n {num_shells} -b {num_bins} {out_dir}/pos.ff {out_dir}/neg.ff > {out_dir}/model.model"
+    )
+
+    return f"{out_dir}/model.model"
+
+
+for num_shells in NUM_SHELLS:
+    for shell_width in SHELL_WIDTH:
+        for num_bins in NUM_BINS:
+            # relative model path
+            model_path = build_feature_model(num_shells, shell_width, num_bins)
