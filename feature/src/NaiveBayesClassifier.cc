5b4e0142edffe50668ef54718289cee2bfc319d4
Matt Katz <mykatz01@gmail.com>
2021-12-02 01:54:00 -0800

    gaussian or something

--- feature/src/NaiveBayesClassifier.cc
+++ feature/src/NaiveBayesClassifier.cc
@@ -6,6 +6,32 @@
 #include "NaiveBayesClassifier.h"
 #include <iostream>
 
+const bool USE_GAUSSIAN = true;
+
+// from https://www.johndcook.com/blog/cpp_phi/
+double cdf(double x)
+{
+    // constants
+    double a1 =  0.254829592;
+    double a2 = -0.284496736;
+    double a3 =  1.421413741;
+    double a4 = -1.453152027;
+    double a5 =  1.061405429;
+    double p  =  0.3275911;
+
+    // Save the sign of x
+    int sign = 1;
+    if (x < 0)
+        sign = -1;
+    x = fabs(x)/sqrt(2.0);
+
+    // A&S formula 7.1.26
+    double t = 1.0/(1.0 + p*x);
+    double y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*exp(-x*x);
+
+    return 0.5*(1.0 + sign*y);
+}
+
 NaiveBayesClassifier::NaiveBayesClassifier( int _numBins, double _pPos ) {
 	numBins    = _numBins;
 	pPos       = _pPos;
@@ -136,7 +162,24 @@ int NaiveBayesClassifier::count( Doubles *values, vector<int> &countBin ) {
 	int bin;
 	int total = 0;
 	for( i = values->begin(); i != values->end(); i++ ) {
-		bin = (int) (((*i) - min) / binSize);
+		if(USE_GAUSSIAN) {
+			//std::cout << "mean: " << mean << "stdev: " << stdev << std::endl;
+			if(stdev == 0) {
+				// fallback to old method when no binsize
+				bin = (int) (((*i) - min) / binSize);
+			} else {
+				double z = ((*i) - mean) / stdev;
+				//std::cout << "z: " << z << std::endl; 
+				double binsize = (double)1 / ((double)numBins);
+				//std::cout << "binsize: " << binsize << std::endl; 
+				double percentile = cdf(z);
+				//std::cout << "percentile: " << percentile << std::endl; 
+				bin = (int) (percentile / binsize);
+				//std::cout << "bin: " << bin << std::endl; 
+			}
+		} else {
+			bin = (int) (((*i) - min) / binSize);
+		}
 		if( bin < 0 )            bin = 0;
 		if( bin > (numBins - 1)) bin = (numBins - 1);
 		countBin[ bin ]++;
