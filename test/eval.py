bd0e94ea2961d32f692b48684d9af85a902f2414
Matt Katz <mykatz01@gmail.com>
2021-12-03 01:27:13 -0800

    Eval script

--- test/eval.py
+++ test/eval.py
@@ -9,23 +9,32 @@ Compare with gold standard
 
 """
 
+import math
+
 GOLD_STANDARD_SITE = "/home/katz/Code/cs279proj/eval/calciums.CA.site"
 # hits file, sorted from highest score (most likely to be a hit) to lowest
 HITS_SORTED_FILE = "/home/katz/Code/cs279proj/out/6-1.25-5-nongauss/hits-r.sorted"
 
-HIT_ANGSTROM_MAX_DISTANCE = 2
+# max distance for each point
+HIT_ANGSTROM_MAX_DISTANCE = 3
+
+TARGET_SPECIFICITY = 0.99
+
+
 
 # dict of pdb id: [list of gold standard sites (3-tuples)]
 gold = {}
+found = set()
 
-# returns true if corresponds to a gold standard hit
+# returns index of point if corresponds to a gold standard hit, -1 otherwise
 def is_hit(pdb_id, x, y, z):
     true_positives = gold[pdb_id]
-    for point in true_positives:
-        dist = ((point[0] - x)**2 + (point[1] - y)**2 + (point[2]-z)**2)**0.5
-        if dist < HIT_ANGSTROM_MAX_DISTANCE:
-            return True
-    return False
+    for i in range(len(true_positives)):
+        point = true_positives[i]
+        max_distance = max([abs(x - point[0]), abs(y - point[1]), abs(z - point[2])])
+        if max_distance < HIT_ANGSTROM_MAX_DISTANCE:
+            return i
+    return -1
 
 with open(GOLD_STANDARD_SITE, "r") as f:
     lines = f.readlines()
@@ -41,8 +50,71 @@ with open(GOLD_STANDARD_SITE, "r") as f:
 
 with open(HITS_SORTED_FILE, "r") as f:
     lines = f.readlines()
-    for line in lines[:20]:
+    total = len(lines)
+    actual_negatives = 0
+    # count number of true positives -- ie calcium binding sites
+    for line in lines:
         line = line.split("\t")
         pdb_id = line[0].split('_')[1].lower()
         x, y, z = (float(line[2]), float(line[3]), float(line[4]))
-        print(pdb_id, x, y, z, is_hit(pdb_id, x, y, z))
+        #print(pdb_id, x, y, z, is_hit(pdb_id, x, y, z))
+        hit_index = is_hit(pdb_id, x, y, z)
+        if hit_index == -1:
+            actual_negatives += 1
+    
+    target_negatives = int((TARGET_SPECIFICITY * actual_negatives) // 1)
+    num_neg = 0
+    cutoff_ind = None
+    i = len(lines)
+    for line in lines[::-1]:
+        i -= 1
+        line = line.split("\t")
+        pdb_id = line[0].split('_')[1].lower()
+        x, y, z = (float(line[2]), float(line[3]), float(line[4]))
+        #print(pdb_id, x, y, z, is_hit(pdb_id, x, y, z))
+        hit_index = is_hit(pdb_id, x, y, z)
+        if hit_index == -1:
+            num_neg += 1
+        if num_neg == target_negatives and not cutoff_ind:
+            cutoff_ind = i
+            break
+
+    i = 0
+    true_pos = 0
+    false_pos = 0
+    true_neg = 0
+    false_neg = 0
+
+    print(cutoff_ind)
+
+    for line in lines:
+        i += 1
+        line = line.split("\t")
+        pdb_id = line[0].split('_')[1].lower()
+        x, y, z = (float(line[2]), float(line[3]), float(line[4]))
+        
+        hit = is_hit(pdb_id, x, y, z) >= 0
+
+        if hit and i <= cutoff_ind:
+            true_pos += 1
+            print(i, true_pos)
+        if hit and i > cutoff_ind:
+            false_neg += 1
+        if not hit and i <= cutoff_ind:
+            false_pos += 1
+        if not hit and i > cutoff_ind:
+            true_neg += 1
+        
+    assert(true_pos + false_pos + true_neg + false_neg)
+
+    print(true_pos)
+    print(false_pos)
+    print(false_neg)
+
+
+    print("Sensitivity: ", true_pos / (true_pos + false_neg))
+    print("Specificity: ", true_neg / (true_neg + false_pos))
+
+    
+
+        
